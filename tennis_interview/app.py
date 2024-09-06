e3610ecc91a3c82b99fcc7690a24e15bf1b4a703
zhoufan <1247714429@qq.com>
2024-09-06 18:47:41 +0800

    support showing summary in markdown

--- tennis_interview/app.py
+++ tennis_interview/app.py
@@ -1,20 +1,26 @@
 from fasthtml.common import *
 from tennis_interview.search import youtube_search
 from tennis_interview.summary import summary as summary_video
+from markdown import markdown
 
 hdrs = (MarkdownJS(), HighlightJS(langs=['python', 'javascript', 'html', 'css']), )
 
 app, rt = fast_app(hdrs=hdrs)
 
-@app.get("/")
-def home():
+summary_content = {
+    "content": "",
+    "generating": False
+}
+
+@rt("/")
+def get():
     inp = Input(id="new-query", name="query", placeholder="", value="us open 2024 interview")
     add = Form(Group(inp, Button("Search")), hx_get="./search", target_id="res-list", hx_swap="innerHTML")
     res_list = Div(id="res-list")
     return Title("Tennis Interview Reading"), Main(H1("Tennis Interview Search"), add, res_list, cls="container")
 
-@app.get("/search")
-def search(query: str):
+@rt("/search")
+def get(query: str):
     results = youtube_search(query)
     videos = results.get('items', [])
     res_list = []
@@ -30,32 +36,30 @@ def search(query: str):
     clear_input = Input(id="new-query", name="query", hx_swap_oob="true")
     return Div(*res_list), clear_input
 
-summary_content = ""
-summary_generating = False
 @threaded
 def get_summary_content(response):
-    global summary_content
     for chunk in response:
         if chunk.choices[0].delta.content is not None:
-            summary_content += chunk.choices[0].delta.content
-    global summary_generating
-    summary_generating = False
-
+            summary_content["content"] += chunk.choices[0].delta.content
+    summary_content["generating"] = False
 
 def SummaryContent():
-    stream_args = {"hx_trigger":"every 0.1s", "hx_swap":"outerHTML", "hx_get":"/summary/content"}
-    return Div(summary_content, **stream_args if summary_generating else {}, cls="marked")
+    stream_args = {"hx_trigger":"every 0.2s", "hx_get":"/summary/content", "hx_swap":"outerHTML"}
+    print(summary_content)
+    generating = 'generating' in summary_content and summary_content['generating']
+    print(generating)
+    return Div(NotStr(markdown(summary_content["content"])), **stream_args if generating else {})
 
-@app.get("/summary/content")
+@rt("/summary/content")
 def get():
     return SummaryContent()
 
-@app.get("/summary/{video_id}")
-def summary(video_id: str):
-    global summary_generating
-    summary_generating = True
+@rt("/summary/{video_id}")
+def get(video_id: str):
+    summary_content["generating"] = True
+    summary_content["content"] = ""
     response = summary_video(video_id)
     get_summary_content(response)
-    return Title("Video Summary"), Main(H1("Video Summary"), SummaryContent())
+    return Title("Video Summary"), Main(SummaryContent())
 
 serve()
