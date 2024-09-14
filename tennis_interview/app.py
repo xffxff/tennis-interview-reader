40b34a6024c3bac9d673fcbf547241478646bf46
zhoufan <1247714429@qq.com>
2024-09-14 17:39:21 +0800

    support choosing search tools

--- tennis_interview/app.py
+++ tennis_interview/app.py
@@ -3,7 +3,7 @@ from fasthtml.common import *
 from markdown import markdown
 from starlette.responses import RedirectResponse
 
-from tennis_interview.search import youtube_api_search as video_search
+from tennis_interview.search import youtube_api_search, duckduckgo_search, serper_api_search
 from tennis_interview.summary import summary as summary_video
 
 load_dotenv()
@@ -33,19 +33,33 @@ summary_content = {"content": "", "generating": False, "cancelled": False}
 
 def SearchPage(query: Optional[str] = None, search_results: list[Video] = None):
     search = Form(
-        Search(
-            Input(
-                type="search",
-                id="new-query",
-                name="query",
-                placeholder="Search for a tennis interview",
-                value=query,
+        Div(
+            Search(
+                Input(
+                    type="search",
+                    id="new-query",
+                    name="query",
+                    placeholder="Search for a tennis interview",
+                    value=query,
+                ),
             ),
+            cls="w-full",
+        ),
+        Div(
+            Select(
+                Option("YouTube API", value="youtube", selected=True),
+                Option("DuckDuckGo", value="duckduckgo"),
+                Option("Serper", value="serper"),
+                name="api",
+                id="search-api",
+                cls="text-sm py-1 text-gray-400"
+            ),
+            cls="w-1/3 mt-0 mx-auto",
         ),
         hx_get="./search",
         target_id="res-list",
         hx_swap="innerHTML",
-        cls="w-full max-w-md",
+        cls="w-full max-w-md flex flex-col",
     )
 
     div_cls = "flex flex-col items-center justify-center w-full"
@@ -93,8 +107,14 @@ def VideoCard(video: Video):
 
 
 @rt("/search")
-def get(query: str, session):
-    results = video_search(query, max_results=12)
+def get(query: str, api: str, session):
+    max_results = 12
+    if api == "youtube":
+        results = youtube_api_search(query, max_results=max_results)
+    elif api == "duckduckgo":
+        results = duckduckgo_search(query, max_results=max_results)
+    elif api == "serper":
+        results = serper_api_search(query, max_results=max_results)
     session["last_query"] = query
     return SearchPage(query,results)
 
